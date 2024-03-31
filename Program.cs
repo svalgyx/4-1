2f49d3054f0a92d233a9195e470fd4a9d6aa3c24
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-03-31 18:46:20 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,21 +9,21 @@ namespace HW3_4
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            SingleArray<int> single_array = new SingleArray<int>(0);
+            SingleArray<int> single_ar = new SingleArray<int>(0);
             for (int i = 0; i < 11; i++)
             {
                 single_array.Add(rnd.Next(1, 100));
             }
-            single_array.ForEach((x) => Console.WriteLine(x));
+            single_ary.ForEach((x) => Console.WriteLine(x));
             Console.WriteLine(single_array.All((x) => x >= 5));
-            int[] array = single_array.Where((x) => x >= 25);
+            int[] array = single_ar.Where((x) => x >= 25);
             foreach (int n in array)
             {
                 Console.WriteLine(n);
             }
-            single_array.Reverse();
-            single_array.ForEach((x) => Console.WriteLine(x));
-            Console.WriteLine(single_array.Min());
+            single_ar.Reverse();
+            single_ar.ForEach((x) => Console.WriteLine(x));
+            Console.WriteLine(single_ar.Min());
             Console.Write("Press enter to exit: ");
             Console.ReadLine();
         }
