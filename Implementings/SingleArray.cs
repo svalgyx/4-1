2b791dfe1a83bd4d31d7ec3e03386ede8246ee5f
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-03-31 22:35:53 +0300

    Update SingleArray.cs

--- Implementings/SingleArray.cs
+++ Implementings/SingleArray.cs
@@ -145,7 +145,7 @@ namespace HW3_4.Implementings
             T min = items[0];
             for (int i = 1; i < size; i++)
             {
-                if (min.CompareTo(items[i]) > 0)
+                if (min.Compare(items[i]) > 0)
                 {
                     min = items[i];
                 }
@@ -158,7 +158,7 @@ namespace HW3_4.Implementings
             T min = items[0];
             for (int i = 1; i < size; i++)
             {
-                if (min.CompareTo(items[i]) < 0)
+                if (min.Compare(items[i]) < 0)
                 {
                     min = items[i];
                 }
