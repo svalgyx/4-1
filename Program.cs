0975ea4f842358aa8f9dbaed79d28394af73bc6c
svalgyx <154222657+svalgyx@users.noreply.github.com>
2024-03-31 18:48:18 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,13 +9,13 @@ namespace HW3_4
         static void Main(string[] args)
         {
             Random rnd = new Random();
-            SingleArray<int> single_ar = new SingleArray<int>(0);
+            Single<int> single_ar = new Single<int>(0);
             for (int i = 0; i < 11; i++)
             {
                 single_array.Add(rnd.Next(1, 100));
             }
             single_ary.ForEach((x) => Console.WriteLine(x));
-            Console.WriteLine(single_array.All((x) => x >= 5));
+            Console.WriteLine(single_ar.All((x) => x >= 5));
             int[] array = single_ar.Where((x) => x >= 25);
             foreach (int n in array)
             {
